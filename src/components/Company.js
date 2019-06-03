71197e53bd111c41f4813a20bee3352b5296a0d9
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-06-03 14:06:40 -0500

    add content to company page

--- src/components/Company.js
+++ src/components/Company.js
@@ -3,9 +3,41 @@ import React, { PureComponent } from 'react';
 export default class Company extends PureComponent {
     render() {
         return (
-            <div className="company-container">
-                <div className="featured-image-wrap">
-                    <img src="./assets/interior.jpg" className="featured-image" alt="car"/>
+            <div className="company__container">
+                <div className="company__background-image-wrap">
+                    <img src="./assets/interior.jpg" className="company__background-image" alt="car"/>
+                </div>
+                <div className="company__portfolio">
+                    <div className="company-portfolio__info-wrap info-odd">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
+                    </div>
+                    <div className="company-portfolio__image-wrap image-odd">
+                        <img src="./assets/employee-1.jpg" className="company-portfolio__image" alt="employee"/>
+                    </div>
+                </div>
+                <div className="company__portfolio">
+                    <div className="company-portfolio__info-wrap info-even">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
+                    </div>
+                    <div className="company-portfolio__image-wrap image-even">
+                        <img src="./assets/employee-2.jpg" className="company-portfolio__image" alt="employee"/>
+                    </div>
+                </div>
+                <div className="company__portfolio">
+                    <div className="company-portfolio__info-wrap info-odd">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
+                    </div>
+                    <div className="company-portfolio__image-wrap image-odd">
+                        <img src="./assets/employee-3.jpg" className="company-portfolio__image" alt="employee"/>
+                    </div>
+                </div>
+                <div className="company__portfolio">
+                    <div className="company-portfolio__info-wrap info-even">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
+                    </div>
+                    <div className="company-portfolio__image-wrap image-even">
+                        <img src="./assets/employee-4.jpg" className="company-portfolio__image" alt="employee"/>
+                    </div>
                 </div>
             </div>
         )
