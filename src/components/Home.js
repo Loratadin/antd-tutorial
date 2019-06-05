28ea42beda82cb63ab4d3108db73e39db433e55b
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-06-05 16:12:09 -0500

    style the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,14 @@ import React, { PureComponent } from 'react';
 export default class Home extends PureComponent {
     render() {
         return (
-            <div className="home-container">
-                <div className="featured-image-wrap">
-                    <img src="./assets/hero.jpg" className="featured-image" alt="car"/>
+            <div className="home__container">
+                <div className="home__featured-image-wrap">
+                    <img src="./assets/hero.jpg" className="home__featured-image" alt="car"/>
+                </div>
+                <div className="home__featured-content-wrap">
+                    <div className="home__featured-content">Premium</div>
+                    <div className="home__featured-content">Car Rental</div>
+                    <div className="home__featured-content">In Dallas</div>
                 </div>
             </div>
         )
