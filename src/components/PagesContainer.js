88c690cfc84a43ffceab643688676fb4f6cfbc68
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-01-25 14:05:06 -0600

    update pages container styles

--- src/components/PagesContainer.js
+++ src/components/PagesContainer.js
@@ -1,27 +1,27 @@
 import React, { PureComponent } from 'react';
 import { Tabs } from 'antd';
-import Home from '../components/Home';
 import Header from '../components/Header';
+import Home from '../components/Home';
 import Company from '../components/Company';
 
-const TabPane = Tabs.TabPane;
+const { TabPane } = Tabs;
 export default class PagesContainer extends PureComponent {
     render() {
         return (
             <div className="pages-container">
-                <Header/>
-                <Tabs className="pages-container__tabs" defaultActiveKey="home" >
-                    <TabPane className="pages-container__tab" tab="Home" key="home">
+                <Header />
+                <Tabs defaultActiveKey="home" >
+                    <TabPane tab="Home" key="home">
                         <Home/>
                     </TabPane>
-                    <TabPane className="pages-container__tab" tab="Cars" key="cars">
-                    Cars
+                    <TabPane tab="Cars" key="cars">
+                        Cars
                     </TabPane>
-                    <TabPane className="pages-container__tab" tab="Company" key="company">
+                    <TabPane tab="Company" key="company">
                         <Company/>
                     </TabPane>
-                    <TabPane className="pages-container__tab" tab="Order" key="order">
-                    Order
+                    <TabPane tab="Order" key="order">
+                        Order
                     </TabPane>
                 </Tabs>
             </div>
