405c33c7410a86187b370bc0798226b0a0d6a96c
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-02-28 10:25:44 -0600

    rearrange cars layout

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -10,16 +10,19 @@ export default class Cars extends PureComponent {
                     <img src="./assets/interior.jpg" className="cars__background-image" alt="car"/>
                 </div>
                 <div className="cars__portfolio-wrap">
-                    {inventory && inventory.map(car => (
-                        <React.Fragment>
-                            <div className="cars-portfolio__info-wrap ">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
-                            </div>
-                            <div className="cars-portfolio__image-wrap" >
-                                <img src={car.imageUrl} className="cars-portfolio__image" alt="car"/>
+                    <div className="cars-portfolio__filters">Filters</div>
+                    <div className="cars-portfolio__results">
+                        {inventory && inventory.map(car => (
+                            <div className="cars-portfolio__record">
+                                <div className="cars-portfolio__record-info-wrap">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius morbi enim nunc faucibus a pellentesque sit.
                                 </div>
-                        </React.Fragment>
-                    ))}
+                                <div className="cars-portfolio__record-image-wrap">
+                                    <img src={car.imageUrl} className="cars-portfolio__image" alt="car"/>
+                                    </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         )
